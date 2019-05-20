e103d9d0fc096e5ce7d06e19e99212ad9c539ffd
kwaneung <whatsup95@naver.com>
2019-05-20 21:52:02 +0900

    Update connection_test.ino

--- Servo/arduino/connection_test/connection_test.ino
+++ Servo/arduino/connection_test/connection_test.ino
@@ -20,10 +20,10 @@ Servo myservo;
 ESP8266WebServer server(80);
 
 
-const char* ssid = "KPU_WiFi938";
-const char* password = "";
+const char* ssid = "iPhone";
+const char* password = "1234567890";
 char path[] = "/";
-char host[] = "3Nyang.gonetis.com";
+char host[] = "172.20.10.3";
 int port = 180;
 
 byte nuidPICC[4];
@@ -42,7 +42,7 @@ WiFiClient client;
 
 void handleON() {
   server.send(200, "text/plane", "Turned ON !!");
-  //Serial.println("Doorlock Open !!");
+  Serial.println("Doorlock Open !!");
   myservo.write(180);
   delay(1500);
   myservo.write(0);
@@ -56,8 +56,8 @@ String recv_data()
         webSocketClient.getData(data);
         if (data.length() > 0)
         {
-          //Serial.print("\nReceived data: ");
-          //Serial.println(data);
+          Serial.print("\nReceived data: ");
+          Serial.println(data);
           return data;
         }
       }
@@ -65,7 +65,7 @@ String recv_data()
 
 void send_uid()
 {
-  //Serial.print("UID 전송\n");
+  Serial.print("UID 전송\n");
   for(int i=0; i<4; i++)
     webSocketClient.sendData(String(rfid.uid.uidByte[i]));
 }
@@ -98,14 +98,14 @@ int door_ctrl(String rcv_data)
   int flag = 0;
   if (rcv_data == "1")
       {
-        //Serial.println("Open");
+        Serial.println("Open");
         myservo.write(180);
         delay(1500);
-        //Serial.println("\Close");
+        Serial.println("\Close");
         myservo.write(0);
         flag = 1;
       }
-      //else Serial.print("\n등록되지 않은 사용자 입니다.\n");
+      else Serial.print("\n등록되지 않은 사용자 입니다.\n");
       return flag;
 }
 
@@ -118,7 +118,7 @@ void setup() {
   //인터럽트 모드에는 FALLING(HIGH->LOW), RISING(LOW->HIGH), 
   //CHANGE(변경시), LOW(신호가 LOW일 경우)
 
-  //Serial.begin(9600);
+  Serial.begin(9600);
   myservo.attach(Smoter);
   SPI.begin(); // SPI 시작
   rfid.PCD_Init(); // RFID 모듈시작
@@ -129,22 +129,22 @@ void setup() {
   //Serial.print(F("Using the following key:"));
   printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
 
-  //Serial.println();
-  //Serial.println();
-  //Serial.print("Connecting to ");
-  //Serial.println(ssid);
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
 
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    //Serial.print(".");
+    Serial.print(".");
   }
 
-  //Serial.println("");
-  //Serial.println("WiFi connected");
-  //Serial.print("IP address: ");
-  //Serial.println(WiFi.localIP());
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
   server.on("/on", handleON);
   server.begin();
   
@@ -165,40 +165,40 @@ void loop() {
   
 
 
-  //Serial.print(F("PICC type: "));
+  Serial.print(F("PICC type: "));
   MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak); //카드의 타입을 읽어옴
-  //Serial.println(rfid.PICC_GetTypeName(piccType));
+  Serial.println(rfid.PICC_GetTypeName(piccType));
 
   // 감지한 ID의 방식이 MIFARE가 아니라면
   if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&
     piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
     piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
-    //Serial.println(F("Your tag is not of type MIFARE Classic."));
+    Serial.println(F("Your tag is not of type MIFARE Classic."));
     return;
   }
-  //Serial.println("");
-  //Serial.print("taging UID : ");
+  Serial.println("");
+  Serial.print("taging UID : ");
   printHex(rfid.uid.uidByte, rfid.uid.size);//태깅된 UID출력
-  //Serial.println("");
+  Serial.println("");
 
   // Connect to the websocket server
   if(connect_web(host, port))
   {
-    //Serial.println("connection successful");
+    Serial.println("connection successful");
   }
   else
   {
-    //Serial.println("connection failed.");
+    Serial.println("connection failed.");
   }
 
   // Handshake with the server
   if(handshake_web(host, path))
   {
-    //Serial.println("handshake successful");
+    Serial.println("handshake successful");
   }
   else
   {
-    //Serial.println("handshake failed.");
+    Serial.println("handshake failed.");
   }
 
 
@@ -222,20 +222,20 @@ void loop() {
       }
     }
     else { //사용자 등록 알고리즘.
-      //Serial.print("\n사용자를 등록합니다.");
+      Serial.print("\n사용자를 등록합니다.");
       webSocketClient.sendData("2");
       send_uid();//uid 전송
       
-//      if (recv_data() == "2")
-//        Serial.print("\n사용자가 성공적으로 등록되었습니다.\n");
-//      else
-//        Serial.print("\n사용자 등록에 실패하였습니다.\n");
+      if (recv_data() == "2")
+        Serial.print("\n사용자가 성공적으로 등록되었습니다.\n");
+      else
+        Serial.print("\n사용자 등록에 실패하였습니다.\n");
       state = LOW;//사용자 등록 마친 후 LOW로 바꿔줘 일반 모드로 들어감.
 
     }
   }
   else {
-    //Serial.println("Client disconnected.");
+    Serial.println("Client disconnected.");
     while (1) {
       // Hang on disconnect.
     }
@@ -248,8 +248,8 @@ void loop() {
 // 카드 ID를 16진수로 바꿔주는 함수
 void printHex(byte *buffer, byte bufferSize) {
   for (byte i = 0; i < bufferSize; i++) {
-    //Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-    //Serial.print(buffer[i], HEX);
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], HEX);
   }
 }
 void interrupt() {
